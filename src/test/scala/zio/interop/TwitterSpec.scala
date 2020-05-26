852363c4325d2cf0f46e40ea16ec21c7a3b09c33
Dejan Mijic <dmijic@acm.org>
2020-05-26 08:39:47 +0200

    Update scalafmt to 2.5.3

--- src/test/scala/zio/interop/TwitterSpec.scala
+++ src/test/scala/zio/interop/TwitterSpec.scala
@@ -13,70 +13,71 @@ import scala.util.{ Failure, Success, Try }
 object TwitterSpec extends DefaultRunnableSpec {
   val runtime = runner.runtime
 
-  override def spec = suite("TwitterSpec")(
-    suite("Task.fromTwitterFuture")(
-      testM("return failing `Task` if future failed.") {
-        val error  = new Exception
-        val future = Task(Future.exception[Int](error))
-        val task   = Task.fromTwitterFuture(future).unit
+  override def spec =
+    suite("TwitterSpec")(
+      suite("Task.fromTwitterFuture")(
+        testM("return failing `Task` if future failed.") {
+          val error  = new Exception
+          val future = Task(Future.exception[Int](error))
+          val task   = Task.fromTwitterFuture(future).unit
 
-        assertM(task.either)(isLeft(equalTo(error)))
-      },
-      testM("return successful `Task` if future succeeded.") {
-        val value  = 10
-        val future = Task(Future.value(value))
-        val task   = Task.fromTwitterFuture(future).option
+          assertM(task.either)(isLeft(equalTo(error)))
+        },
+        testM("return successful `Task` if future succeeded.") {
+          val value  = 10
+          val future = Task(Future.value(value))
+          val task   = Task.fromTwitterFuture(future).option
 
-        assertM(task)(isSome(equalTo(value)))
-      },
-      testM("ensure future is interrupted together with task.") {
-        val value = new AtomicInteger(0)
+          assertM(task)(isSome(equalTo(value)))
+        },
+        testM("ensure future is interrupted together with task.") {
+          val value = new AtomicInteger(0)
 
-        val promise = new Promise[Unit] with Promise.InterruptHandler {
-          override protected def onInterrupt(t: Throwable): Unit = setException(t)
-        }
-
-        val future = Task(promise.flatMap(_ => Future(value.incrementAndGet())))
-
-        val task =
-          (for {
-            fiber <- Task.fromTwitterFuture(future).fork
-            _     <- fiber.interrupt
-            _     <- Task.effect(promise.setDone())
-            a     <- fiber.await
-          } yield a).fold(_ => false, exit => exit.toEither.isLeft)
-
-        task.map(b => assert(b)(isTrue) && assert(value.get())(equalTo(0)))
-      }
-    ),
-    suite("Runtime.unsafeRunToTwitterFuture")(
-      test("return successful `Future` if Task evaluation succeeded.") {
-        assert(Await.result(runtime.unsafeRunToTwitterFuture(Task.succeed(2))))(equalTo(2))
-      },
-      test("return failed `Future` if Task evaluation failed.") {
-        val e    = new Throwable
-        val task = Task.fail(e).unit
-        val result =
-          Try(Await.result(runtime.unsafeRunToTwitterFuture(task))) match {
-            case Failure(exception) => Some(exception)
-            case Success(_)         => None
+          val promise = new Promise[Unit] with Promise.InterruptHandler {
+            override protected def onInterrupt(t: Throwable): Unit = setException(t)
           }
 
-        assert(result)(isSome(equalTo(e)))
-      },
-      testM("ensure Task evaluation is interrupted together with Future.") {
-        val value = new AtomicInteger(0)
-        val ex    = new Exception
-        val task: ZIO[Any, Throwable, Future[Int]] = for {
-          promise <- zio.Promise.make[Throwable, Int]
-          t       = promise.await.flatMap(_ => Task.effectTotal(value.incrementAndGet()))
-          future  = runtime.unsafeRunToTwitterFuture(t)
-          _       = future.raise(ex)
-          _       <- promise.succeed(1)
-        } yield future
+          val future = Task(promise.flatMap(_ => Future(value.incrementAndGet())))
 
-        assertM(task.map(Await.result(_)).run)(isInterrupted).map(_ && assert(value.get)(equalTo(0)))
-      }
+          val task =
+            (for {
+              fiber <- Task.fromTwitterFuture(future).fork
+              _     <- fiber.interrupt
+              _     <- Task.effect(promise.setDone())
+              a     <- fiber.await
+            } yield a).fold(_ => false, exit => exit.toEither.isLeft)
+
+          task.map(b => assert(b)(isTrue) && assert(value.get())(equalTo(0)))
+        }
+      ),
+      suite("Runtime.unsafeRunToTwitterFuture")(
+        test("return successful `Future` if Task evaluation succeeded.") {
+          assert(Await.result(runtime.unsafeRunToTwitterFuture(Task.succeed(2))))(equalTo(2))
+        },
+        test("return failed `Future` if Task evaluation failed.") {
+          val e      = new Throwable
+          val task   = Task.fail(e).unit
+          val result =
+            Try(Await.result(runtime.unsafeRunToTwitterFuture(task))) match {
+              case Failure(exception) => Some(exception)
+              case Success(_)         => None
+            }
+
+          assert(result)(isSome(equalTo(e)))
+        },
+        testM("ensure Task evaluation is interrupted together with Future.") {
+          val value                                  = new AtomicInteger(0)
+          val ex                                     = new Exception
+          val task: ZIO[Any, Throwable, Future[Int]] = for {
+            promise <- zio.Promise.make[Throwable, Int]
+            t        = promise.await.flatMap(_ => Task.effectTotal(value.incrementAndGet()))
+            future   = runtime.unsafeRunToTwitterFuture(t)
+            _        = future.raise(ex)
+            _       <- promise.succeed(1)
+          } yield future
+
+          assertM(task.map(Await.result(_)).run)(isInterrupted).map(_ && assert(value.get)(equalTo(0)))
+        }
+      )
     )
-  )
 }
