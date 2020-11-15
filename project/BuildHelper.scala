cedb2e090890f208aec9bbd44c263fee27048c64
Dejan Mijic <dmijic@acm.org>
2020-11-15 22:37:03 +0100

    Upgrade scala versions

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -75,7 +75,7 @@ object BuildHelper {
     Seq(
       name := s"$prjName",
       scalacOptions := stdOptions,
-      crossScalaVersions := Seq("2.13.0", "2.12.8", "2.11.12"),
+      crossScalaVersions := Seq("2.13.3", "2.12.12", "2.11.12"),
       scalaVersion in ThisBuild := crossScalaVersions.value.head,
       scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
       libraryDependencies ++= compileOnlyDeps ++ testDeps ++ Seq(
