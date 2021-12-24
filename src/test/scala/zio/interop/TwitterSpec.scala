5494597e58cc9b30e421cb2e7fe93a6461fdfd97
Dejan Mijic <dmijic@acm.org>
2021-12-24 13:23:33 +0100

    Revert "Sleep to allow interrupts propagation"
    This reverts commit d820225c25a2c5a3746af0f191c5e9220242a27c.

--- src/test/scala/zio/interop/TwitterSpec.scala
+++ src/test/scala/zio/interop/TwitterSpec.scala
@@ -37,7 +37,6 @@ object TwitterSpec extends DefaultRunnableSpec {
             ref   <- UIO(new AtomicInteger(0))
             fiber <- Task.fromTwitterFuture(infiniteFuture(ref)).fork
             _     <- fiber.interrupt
-            _     <- Live.live(clock.sleep(10.millis))
             v1    <- UIO(ref.get)
             _     <- Live.live(clock.sleep(10.millis))
             v2    <- UIO(ref.get)
