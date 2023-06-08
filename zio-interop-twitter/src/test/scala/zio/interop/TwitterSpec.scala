d8c42653a3c2a3bfab9aacc2a315db167e330c36
Victor Borja <vborja@apache.org>
2023-06-08 05:00:11 -0600

    [wip] updating for zio 2.0.1 (#271)
    Co-authored-by: Daniel Vigovszky <daniel.vigovszky@gmail.com>

--- zio-interop-twitter/src/test/scala/zio/interop/TwitterSpec.scala
+++ zio-interop-twitter/src/test/scala/zio/interop/TwitterSpec.scala
@@ -1,79 +1,89 @@
 package zio.interop
 
 import com.twitter.util.{ Await, Future, FuturePool }
-import java.util.concurrent.atomic.AtomicInteger
-import zio.{ Executor, RuntimeConfig, Task, UIO }
 import zio.interop.twitter._
-import zio.test._
 import zio.test.Assertion._
 import zio.test.TestAspect.{ nonFlaky, sequential }
+import zio.test._
+import zio._
 
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
 import scala.concurrent.ExecutionContext
 
-object TwitterSpec extends DefaultRunnableSpec {
-  override val runner = {
-    val ec       = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(2))
-    val executor = Executor.fromExecutionContext(RuntimeConfig.defaultYieldOpCount)(ec)
+object TwitterSpec extends ZIOSpecDefault {
 
-    defaultTestRunner.withRuntimeConfig(_.copy(executor = executor))
+  val customExecutor: Executor = {
+    val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(2))
+    Executor.fromExecutionContext(ec)
   }
 
-  val runtime = runner.runtime
+  val customExecutorLayer: ZLayer[Any, Nothing, Any] =
+    Runtime.setExecutor(customExecutor) to Runtime.setBlockingExecutor(customExecutor)
+
+  override val bootstrap: ZLayer[Any, Any, TestEnvironment] =
+    testEnvironment andTo customExecutorLayer
 
   def spec =
     suite("TwitterSpec")(
-      suite("Task.fromTwitterFuture")(
+      suite("ZIO.fromTwitterFuture")(
         test("lifts failed futures") {
           for {
-            error  <- UIO(new Exception)
-            result <- Task.fromTwitterFuture(Future.exception(error)).either
+            error  <- ZIO.succeed(new Exception)
+            result <- ZIO.fromTwitterFuture(Future.exception(error)).either
           } yield assert(result)(isLeft(equalTo(error)))
         },
         test("lifts successful futures") {
           for {
-            value  <- UIO(10)
-            result <- Task.fromTwitterFuture(Future.value(value))
+            value  <- ZIO.succeed(10)
+            result <- ZIO.fromTwitterFuture(Future.value(value))
           } yield assert(result)(equalTo(value))
         },
         test("ensures future is interrupted") {
-          val pool = FuturePool.interruptible(runtime.runtimeConfig.executor.asExecutionContextExecutorService)
+          val pool = FuturePool.interruptible(customExecutor.asExecutionContextExecutorService)
 
           def infiniteFuture(ref: AtomicInteger): Future[Nothing] =
             pool(ref.getAndIncrement()).flatMap(_ => infiniteFuture(ref))
 
           for {
-            ref   <- UIO(new AtomicInteger(0))
-            fiber <- Task.fromTwitterFuture(infiniteFuture(ref)).fork
+            ref   <- ZIO.succeed(new AtomicInteger(0))
+            fiber <- ZIO.fromTwitterFuture(infiniteFuture(ref)).fork
             _     <- fiber.interrupt
-            v1    <- UIO(ref.get)
-            v2    <- UIO(ref.get)
+            v1    <- ZIO.succeed(ref.get)
+            v2    <- ZIO.succeed(ref.get)
           } yield assert(v1)(equalTo(v2))
         } @@ nonFlaky(100000)
       ),
-      suite("Runtime.unsafeRunToTwitterFuture")(
+      suite("Runtime.unsafe.runToTwitterFuture")(
         test("produces successful futures if Task evaluation succeeds") {
           for {
-            value  <- UIO(10)
-            result <- Task.attempt(unsafeAwait(UIO(value)))
+            value  <- ZIO.succeed(10)
+            result <- ZIO.attempt(unsafeAwait(ZIO.succeed(value)))
           } yield assert(result)(equalTo(value))
         },
         test("produces failed futures if Task evaluation failed") {
           for {
-            error  <- UIO(new Exception)
-            result <- Task.attempt(unsafeAwait(Task.fail(error))).either
+            error  <- ZIO.succeed(new Exception)
+            result <- ZIO.attempt(unsafeAwait(ZIO.fail(error))).either
           } yield assert(result)(isLeft(equalTo(error)))
         },
         test("ensures task is interrupted") {
           for {
-            future <- Task.attempt(runtime.unsafeRunToTwitterFuture(UIO.never))
-            _      <- Task.attempt(future.raise(new Exception))
-            status <- Task.attempt(Await.result(future)).either
+            future <- ZIO.attempt(unsafeRunToTwitterFuture(ZIO.never))
+            _      <- ZIO.attempt(future.raise(new Exception))
+            status <- ZIO.attempt(Await.result(future)).either
           } yield assert(status)(isLeft)
         } @@ nonFlaky(100000)
       )
     ) @@ sequential
 
   private def unsafeAwait[A](task: Task[A]): A =
-    Await.result(runtime.unsafeRunToTwitterFuture(task))
+    Unsafe.unsafe { implicit unsafe =>
+      Await.result(runtime.unsafe.runToTwitterFuture(task.provideSomeLayer(customExecutorLayer)))
+    }
+
+  private def unsafeRunToTwitterFuture[A](task: Task[A]): Future[A] =
+    Unsafe.unsafe { implicit unsafe =>
+      runtime.unsafe.runToTwitterFuture(task.provideSomeLayer(customExecutorLayer))
+    }
 }
